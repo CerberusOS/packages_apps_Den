ViperOS/android_packages_apps_Venom

[Squash] Add font manager
Add Font preference for font manager
From: bigrushdog <randall.rushing@gmail.com>

Add progress for font apply [2/2]
- Remove logging
- Change string to something that sounds better
From: maxwen <max.weninger@gmail.com>

src/com/viper/venom/fragments/FontDialogPreference.java
@@ -1,0 +1,96 @@
+/*
+ * Copyright (C) 2018 The Dirty Unicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.viper.venom.fragments;
+
+import android.app.AlertDialog.Builder;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.FontInfo;
+import android.content.IFontService;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.util.AttributeSet;
+
+import com.android.settingslib.CustomDialogPreference;
+import com.android.settings.R;
+
+public class FontDialogPreference extends CustomDialogPreference {
+    private static final String TAG = "FontDialogPreference";
+    private Context mContext;
+    private IFontService mFontService;
+    private ProgressDialog mProgressDialog;
+
+    public FontDialogPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mContext = context;
+        mFontService = IFontService.Stub.asInterface(
+                ServiceManager.getService("fontservice"));
+    }
+
+    @Override
+    protected void onPrepareDialogBuilder(Builder builder,
+            DialogInterface.OnClickListener listener) {
+        super.onPrepareDialogBuilder(builder, listener);
+        FontListAdapter adapter = new FontListAdapter(mContext);
+        DialogInterface.OnClickListener l = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                FontInfo info = adapter.getItem(which);
+                try {
+                    startProgress();
+                    mFontService.applyFont(info);
+                } catch (RemoteException e) {
+                    stopProgress();
+                }
+            }
+        };
+        builder.setIcon(R.drawable.font_dialog_icon);
+        builder.setAdapter(adapter, l);
+        builder.setCancelable(false);
+        builder.setNegativeButton(mContext.getString(com.android.internal.R.string.cancel),
+                listener);
+    }
+
+    @Override
+    protected void onClick(DialogInterface dialog, int which) {
+        if (which == DialogInterface.BUTTON_NEGATIVE) {
+            dialog.dismiss();
+        }
+    }
+
+    private void startProgress() {
+        if(mProgressDialog != null) {
+            stopProgress();
+        }
+        mProgressDialog = new ProgressDialog(mContext);
+        mProgressDialog.setIndeterminate(true);
+        mProgressDialog.setTitle(mContext.getString(R.string.font_picker_title));
+        mProgressDialog.setMessage(mContext.getString(R.string.font_picker_progress));
+        mProgressDialog.setCancelable(false);
+        mProgressDialog.setCanceledOnTouchOutside(false);
+        mProgressDialog.show();
+    }
+
+    public void stopProgress() {
+        if (mProgressDialog != null) {
+            mProgressDialog.dismiss();
+            mProgressDialog = null;
+        }
+    }
+}

src/com/viper/venom/fragments/FontListAdapter.java
@@ -1,0 +1,156 @@
+/*
+ * Copyright (C) 2018 The Dirty Unicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.viper.venom.fragments;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+
+import com.android.settings.R;
+
+import android.content.Context;
+import android.content.FontInfo;
+import android.content.IFontService;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class FontListAdapter extends BaseAdapter {
+    private static final String TAG = "FontListAdapter";
+
+    private IFontService mFontService;
+    private List<FontInfo> mFontInfo = new ArrayList<FontInfo>();
+    private Context mContext;
+    private LayoutInflater mInflater;
+    private PackageManager mPm;
+
+    public FontListAdapter(Context context) {
+        mContext = context;
+        mInflater = LayoutInflater.from(context);
+        mFontService = IFontService.Stub.asInterface(
+                ServiceManager.getService("fontservice"));
+        mPm = context.getPackageManager();
+        loadFontList();
+    }
+
+    public void loadFontList() {
+        mFontInfo.clear();
+        try {
+            Map<String, List<FontInfo>> fontMap = mFontService.getAllFonts();
+            for (Map.Entry<String, List<FontInfo>> entry : fontMap.entrySet()) {
+                String packageName = entry.getKey();
+                List<FontInfo> fonts = entry.getValue();
+                // manually add system font after we sort
+                if (TextUtils.equals(packageName, FontInfo.DEFAULT_FONT_PACKAGE)) {
+                    continue;
+                }
+                for (FontInfo font : fonts) {
+                    mFontInfo.add(new FontInfo(font));
+                }
+            }
+            Collections.sort(mFontInfo);
+        } catch (RemoteException e) {
+            Log.e(TAG, "Error in populating list");
+        }
+        mFontInfo.add(0, FontInfo.getDefaultFontInfo());
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return mFontInfo.size();
+    }
+
+    @Override
+    public FontInfo getItem(int position) {
+        return mFontInfo.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        final Context ctx = mContext;
+        if (convertView != null) {
+            holder = (ViewHolder) convertView.getTag();
+        } else {
+            convertView = mInflater.inflate(R.layout.font_item, null, false);
+            holder = new ViewHolder();
+            convertView.setTag(holder);
+            holder.title = (TextView) convertView.findViewById(com.android.internal.R.id.title);
+            holder.summary = (TextView) convertView
+                    .findViewById(com.android.internal.R.id.summary);
+            holder.icon = (ImageView) convertView.findViewById(R.id.icon);
+        }
+        FontInfo info = getItem(position);
+        Typeface.Builder builder = new Typeface.Builder(info.previewPath);
+        Typeface tf = builder.build();
+        holder.title.setTypeface(tf);
+        holder.title.setText(info.fontName.replace("_", " "));
+        holder.summary.setTypeface(tf);
+        holder.summary.setText(mContext.getString(R.string.font_picker_pangram));
+        holder.icon.setImageDrawable(getPackageDrawable(info.packageName));
+        return convertView;
+    }
+
+    private String getPackageLabel(String packageName) {
+        String label = null;
+        if (packageName.equals("android")) {
+            return "Android";
+        }
+        try {
+            ApplicationInfo info = mPm.getApplicationInfo(packageName, 0);
+            label = (String) info.loadLabel(mPm);
+        } finally {
+            return label;
+        }
+    }
+
+    private Drawable getPackageDrawable(String packageName) {
+        Drawable icon = null;
+        try {
+            ApplicationInfo info = mPm.getApplicationInfo(packageName, 0);
+            icon = info.loadIcon(mPm);
+        } finally {
+            return icon;
+        }
+    }
+
+    private static class ViewHolder {
+        TextView title;
+        TextView summary;
+        ImageView icon;
+    }
+}

CerberusOS/packages_apps_Den

src/com/cerberusos/den/preference/FontDialogPreference.java
@@ -1,0 +1,97 @@
+/*
+ * Copyright (C) 2018 The Dirty Unicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.cerberusos.den.preference;
+
+import android.app.AlertDialog.Builder;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.FontInfo;
+import android.content.IFontService;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.util.AttributeSet;
+
+import com.android.settingslib.CustomDialogPreference;
+import com.cerberusos.den.R;
+
+public class FontDialogPreference extends CustomDialogPreference {
+    private static final String TAG = "FontDialogPreference";
+    private Context mContext;
+    private IFontService mFontService;
+    private ProgressDialog mProgressDialog;
+
+    public FontDialogPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mContext = context;
+        mFontService = IFontService.Stub.asInterface(
+                ServiceManager.getService("fontservice"));
+    }
+
+    @Override
+    protected void onPrepareDialogBuilder(Builder builder,
+            DialogInterface.OnClickListener listener) {
+        super.onPrepareDialogBuilder(builder, listener);
+        FontListAdapter adapter = new FontListAdapter(mContext);
+        DialogInterface.OnClickListener l = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                FontInfo info = adapter.getItem(which);
+                try {
+                    startProgress();
+                    mFontService.applyFont(info);
+                } catch (RemoteException e) {
+                    stopProgress();
+                }
+            }
+        };
+        builder.setIcon(R.drawable.font_dialog_icon);
+        builder.setAdapter(adapter, l);
+        builder.setCancelable(false);
+        builder.setNegativeButton(mContext.getString(com.android.internal.R.string.cancel),
+                listener);
+    }
+
+    @Override
+    protected void onClick(DialogInterface dialog, int which) {
+        if (which == DialogInterface.BUTTON_NEGATIVE) {
+            dialog.dismiss();
+        }
+    }
+
+    private void startProgress() {
+        if(mProgressDialog != null) {
+            stopProgress();
+        }
+        mProgressDialog = new ProgressDialog(mContext);
+        mProgressDialog.setIndeterminate(true);
+        mProgressDialog.setTitle(mContext.getString(R.string.font_picker_title));
+        mProgressDialog.setMessage(mContext.getString(R.string.font_picker_progress));
+        mProgressDialog.setCancelable(false);
+        mProgressDialog.setCanceledOnTouchOutside(false);
+        mProgressDialog.show();
+    }
+
+    public void stopProgress() {
+        if (mProgressDialog != null) {
+            mProgressDialog.dismiss();
+            mProgressDialog = null;
+        }
+    }
+}
+

src/com/cerberusos/den/preference/FontListAdapter.java
@@ -1,0 +1,156 @@
+/*
+ * Copyright (C) 2018 The Dirty Unicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.cerberusos.den.preference;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+
+import com.cerberusos.den.R;
+
+import android.content.Context;
+import android.content.FontInfo;
+import android.content.IFontService;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class FontListAdapter extends BaseAdapter {
+    private static final String TAG = "FontListAdapter";
+
+    private IFontService mFontService;
+    private List<FontInfo> mFontInfo = new ArrayList<FontInfo>();
+    private Context mContext;
+    private LayoutInflater mInflater;
+    private PackageManager mPm;
+
+    public FontListAdapter(Context context) {
+        mContext = context;
+        mInflater = LayoutInflater.from(context);
+        mFontService = IFontService.Stub.asInterface(
+                ServiceManager.getService("fontservice"));
+        mPm = context.getPackageManager();
+        loadFontList();
+    }
+
+    public void loadFontList() {
+        mFontInfo.clear();
+        try {
+            Map<String, List<FontInfo>> fontMap = mFontService.getAllFonts();
+            for (Map.Entry<String, List<FontInfo>> entry : fontMap.entrySet()) {
+                String packageName = entry.getKey();
+                List<FontInfo> fonts = entry.getValue();
+                // manually add system font after we sort
+                if (TextUtils.equals(packageName, FontInfo.DEFAULT_FONT_PACKAGE)) {
+                    continue;
+                }
+                for (FontInfo font : fonts) {
+                    mFontInfo.add(new FontInfo(font));
+                }
+            }
+            Collections.sort(mFontInfo);
+        } catch (RemoteException e) {
+            Log.e(TAG, "Error in populating list");
+        }
+        mFontInfo.add(0, FontInfo.getDefaultFontInfo());
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return mFontInfo.size();
+    }
+
+    @Override
+    public FontInfo getItem(int position) {
+        return mFontInfo.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        final Context ctx = mContext;
+        if (convertView != null) {
+            holder = (ViewHolder) convertView.getTag();
+        } else {
+            convertView = mInflater.inflate(R.layout.font_item, null, false);
+            holder = new ViewHolder();
+            convertView.setTag(holder);
+            holder.title = (TextView) convertView.findViewById(com.android.internal.R.id.title);
+            holder.summary = (TextView) convertView
+                    .findViewById(com.android.internal.R.id.summary);
+            holder.icon = (ImageView) convertView.findViewById(R.id.icon);
+        }
+        FontInfo info = getItem(position);
+        Typeface.Builder builder = new Typeface.Builder(info.previewPath);
+        Typeface tf = builder.build();
+        holder.title.setTypeface(tf);
+        holder.title.setText(info.fontName.replace("_", " "));
+        holder.summary.setTypeface(tf);
+        holder.summary.setText(mContext.getString(R.string.font_picker_pangram));
+        holder.icon.setImageDrawable(getPackageDrawable(info.packageName));
+        return convertView;
+    }
+
+    private String getPackageLabel(String packageName) {
+        String label = null;
+        if (packageName.equals("android")) {
+            return "Android";
+        }
+        try {
+            ApplicationInfo info = mPm.getApplicationInfo(packageName, 0);
+            label = (String) info.loadLabel(mPm);
+        } finally {
+            return label;
+        }
+    }
+
+    private Drawable getPackageDrawable(String packageName) {
+        Drawable icon = null;
+        try {
+            ApplicationInfo info = mPm.getApplicationInfo(packageName, 0);
+            icon = info.loadIcon(mPm);
+        } finally {
+            return icon;
+        }
+    }
+
+    private static class ViewHolder {
+        TextView title;
+        TextView summary;
+        ImageView icon;
+    }
+}